c68d5389de3c447ed4deb05c10d120f85ffd98cd
sherrerap <sergio.herrera2@alu.uclm.es>
2021-05-10 21:14:22 +0200

    Añadido programa calculadora

---
+++ app/test_calculadora.py
@@ -0,0 +1,13 @@
+from calculadora import *
+
+def test_suma():
+    assert suma(3, 6) == 9
+
+def test_resta():
+    assert resta(6, 2) == 4
+
+def test_multiplicacion():
+    assert multiplicacion(3, 4) == 12
+
+def test_division():
+    assert division(8, 4) == 2
